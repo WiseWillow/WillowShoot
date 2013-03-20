2e2daac5fb6936d5f01ea8c633631b4dc49ceea6
Arkadiusz <faumar@gmail.com>
2013-03-20 22:37:33 +0100

    Powitanie

--- Tests/Test/main.cpp
+++ Tests/Test/main.cpp
@@ -1,8 +1,9 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    
+    std::cout << "Witam";//Arek
     return a.exec();
 }
