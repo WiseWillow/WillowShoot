3999b68e9490e8b7c89e741649d5e3ff71b715aa
Arkadiusz <faumar@gmail.com>
2013-03-20 23:06:25 +0100

    message

--- Tests/Test/main.cpp
+++ Tests/Test/main.cpp
@@ -9,6 +9,6 @@ int main(int argc, char *argv[])
 
     cout << "Witam"<< endl;//Arek
     cout << "Arek, Ty wiesz kim jestes, nie?" << endl;
-
+    cout << "Nie podskakuj, bo dzieki temu, ze dodalem iostrema masz w ogole glos" << endl;
     return a.exec();
 }
